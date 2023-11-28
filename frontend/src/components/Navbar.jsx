633c1588b3bf0c806aa4d0a3cb61351e8d6a5b1e
Sourav Biswas <sourav.sb56@gmail.com>
2023-11-28 15:58:04 +0530

    Login page UI update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import { Link as ChakraLink, LinkProps } from "@chakra-ui/react";
+import { Flex, Spacer, Container, Box } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <div>
-      <ChakraLink as={ReactRouterLink} to="/">
-        Home
-      </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/profile">
-      Profile
-      </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/login">
-      Login
-      </ChakraLink>
-    </div>
+      <Flex minWidth="max-content" p="1rem 2rem" boxShadow="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+        <Box>
+          <ChakraLink as={ReactRouterLink} to="/">
+            Home
+          </ChakraLink>
+        </Box>
+        <Spacer />
+        <Box>
+          <ChakraLink as={ReactRouterLink} to="/profile">
+            Profile
+          </ChakraLink>
+        </Box>
+        <Spacer />
+        <Box>
+          <ChakraLink as={ReactRouterLink} to="/login">
+            Login
+          </ChakraLink>
+        </Box>
+      </Flex>
   );
 };
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Container,
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement
+} from "@chakra-ui/react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const [show, setShow] = useState(false)
+  const handlePassword = () => setShow(!show)
+
+  const handleChange = (e) => {
+    setLoginData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginData);
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            name="email"
+            value={loginData.email}
+            onChange={handleChange}
+            placeholder="Enter email"
+          />
+        </FormControl>
+        <FormControl>
+          <InputGroup size="md">
+            <Input
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              name="password"
+              value={loginData.password}
+              onChange={handleChange}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handlePassword}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+
+        <Button colorScheme="teal" type="submit">
+          Button
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
